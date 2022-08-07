bbae5ee43c0a3eebb076dce68917c0acd40e403c
Juan Lara <max_lara3005@hotmail.com>
2022-08-06 23:31:52 -0500

    CRUD persona

--- src/main/java/mx/com/gm/ControladorMVC.java
+++ src/main/java/mx/com/gm/ControladorMVC.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ui.Model;
 import mx.com.gm.service.PersonaService;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  *
@@ -46,6 +47,30 @@ public class ControladorMVC {
         model.addAttribute("persona", persona);
         log.info("Ejecutando el controlador Spring MVC");
         return "index";
+    }
+    
+    @GetMapping("/agregar")
+    public String agregar (Persona persona){
+        return "modificar";
+    }
+    
+    @PostMapping("/guardar")
+    public String guardar(Persona persona){
+        personaService.guardar(persona);
+        return "redirect:/mvc";
+    }
+    
+    @GetMapping("/editar/{idPersona}")
+    public String editar(Persona persona, Model model){
+        persona = personaService.encontrarPersona(persona);
+        model.addAttribute("persona", persona);
+        return "modificar";
+    }
+    
+    @GetMapping("/eliminar/{idPersona}")
+    public String eliminar(Persona persona){
+        personaService.eliminar(persona);
+        return "redirect:/mvc";
     }
     
 }
